456bdc2a82cb401d34c8c837a981242b87c76375
Anna <acdodd17@gmail.com>
2022-01-19 08:29:47 -0500

    database and sever connection

---
+++ server.js
@@ -0,0 +1,13 @@
+const sequelize = require('./config/connection');
+
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+//app.use(express.static(path.join(__dirname, 'public')));
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+});
