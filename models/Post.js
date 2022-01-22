f767dacaebb7668bb1641b1ca72ebea729a5416d
Anna <acdodd17@gmail.com>
2022-01-22 06:07:14 -0500

    User and Post models

--- models/Post.js
+++ models/Post.js
@@ -1,3 +1,57 @@
-// model will include user_id
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Post extends Model {};
+
 
 // name, vintage, source, type: red, white, sparkling, rose, img 
+
+// create fields/columns for Post model
+Post.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      wine_name: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      Wine_vintage: {
+        type: DataTypes.NUMBER,
+        validate: {
+            len: [4]
+        }
+      },
+      wine_source: {
+        type: DataTypes.STRING,
+      },
+      wine_type: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      img_url: {
+        type: DataTypes.STRING,
+        validate: {
+          isURL: true
+        }
+      },
+      user_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'user',
+          key: 'id'
+        }
+      }
+    },
+    {
+      sequelize,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'post'
+    }
+  );
+  
+  module.exports = Post;
