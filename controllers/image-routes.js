ef26d23db6de2cd313a4e24efd3bf7c4e8900905
Anna <acdodd17@gmail.com>
2022-01-25 17:40:44 -0500

    file upload

--- controllers/image-routes.js
+++ controllers/image-routes.js
@@ -1,60 +1,18 @@
 const router = require('express').Router();
-const cloudinary = require('../utils/cloudinary');
-const upload = require('../utils/multer');
+const fileUpload = require('express-fileupload');
 
-// post request
-router.post('/', upload.single('image'), async (req, res) => {
-    try {
-        const result = await cloudinary.uploader.upload(req.file.path)
-        res.json(result);
-    } catch (err) {
-        console.log(err)
+router.get('/dashboard', (req, res) => {
+    res.render('dashboard')
+})
+
+router.post('/dashboard', (req, res) => {
+
+    if(!req.files || Object.keys(req.files).lenghth === 0) {
+        return res.status(400).send('No files were uploaded.');
     }
+
+    const file = req.files.file;
+    console.log(sampleFile);
 })
 
 module.exports = router;
-
-
-
-// //import firebase 
-// const firebase = require('firebase');
-
-
-
-// // Set the configuration for your app with env variables
-// const firebaseConfig = {
-//     apiKey: process.env.API_KEY,
-//     authDomain: process.env.AUTH_DOMAIN,
-//     projectId: process.env.PROJECT_ID,
-//     storageBucket: process.env.STORAGE_BUCKET,
-//     appId: process.env.APP_ID,
-//     measurementId: process.env.MEASUREMENT_ID
-// };
-
-// //initialize app with config
-// firebase.initializeApp(firebaseConfig);
-
-// // upload file function
-// function uploadFile (path, file) {
-//     // Get a reference to the storage service, which is used to create references in your storage bucket
-//     console.log(file.originalname);
-//     var storage = firebase.storage();
-//     var storageRef = storage.ref();
-//     var filesRef = storageRef.child(`${path}/${file.originalname}`);
-//     var metadata = {
-//         contentType: 'image/jpg'
-//     }
-//     // upload file method
-//     return filesRef.put(file.buffer, metadata).then((snapshot) => {
-//         console.log("Uploaded a file!")
-//     })
-// }
-
-// // connecting to router to respond to post request with upload id
-// router.post('/:id', upload.single('file'), (req, res) => {
-//     console.log(req.file);
-//     uploadFile(req.params.id, req.file).then((result) => {
-//         res.send(result);
-//     })
-// });
-
--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -5,26 +5,9 @@ const imgUpload = document.getElementById('image');
 
 async function uploadImg(event) {
     event.preventDefault();
-    const file = event.target.files[0];
-    const formData = new FormData();
-    formData.append('file', file);
-    formData.append('upload_preset', 'blcqc8pz');
-
-    const response = await fetch('/upload', {
-        url: 'https://api.cloudinary.com/v1_1/dngr1unbt',
-        method: 'post', 
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded'
-        }, 
-        data: formData
-    })
-
-    if (response.ok) {
-        console.log('success');
-        document.location.replace('/dashboard');
-    } else {
-        alert(response.statusText);
-    }
+    const files = event.target.files[0];
+    const reader = new FileReader
+    reader.onload  
     
 ;}
 
