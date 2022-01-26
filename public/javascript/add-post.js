d96429faa773aac54a6cc82b110e147651a2c3c7
Anna <acdodd17@gmail.com>
2022-01-26 09:56:58 -0500

    dashboard style for notes

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -1,36 +1,34 @@
 async function newWineHandler(event) {
-    event.preventDefault();
+  event.preventDefault();
+
+  const wine_name = document.querySelector('input[name="wine-name"]').value;
+  const wine_type = document.querySelector('#wine-type option:checked').value;
+  const wine_vintage = document.querySelector('input[name="wine-vintage"]').value;
+  const wine_source = document.querySelector('input[name="wine-source"]').value;
+  const wine_notes = document.querySelector('input[name="wine-notes"]').value;
   
-    const wine_name = document.querySelector('input[name="wine-name"]').value;
-    const wine_type = document.querySelector('#wine-type option:checked').value;
-    const wine_vintage = document.querySelector('input[name="wine-vintage"]').value;
-    const wine_source = document.querySelector('input[name="wine-source"]').value;
-    const wine_notes = document.querySelector('input[name="wine-notes"]').value;
-    
 
-    const response = await fetch(`/api/posts`, {
-        method: 'POST',
-        body: JSON.stringify({
-          wine_name,
-          wine_type,
-          wine_vintage,
-          wine_source,
-          wine_notes
-        }),
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      });
+  const response = await fetch(`/api/posts`, {
+    method: 'POST',
+    body: JSON.stringify({
+      wine_name,
+      wine_type,
+      wine_vintage,
+      wine_source,
+      wine_notes
+    }),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
 
 
-      if (response.ok) {
-        document.location.replace('/dashboard');
-      } else {
-        alert(response.statusText);
-      }
+  if (response.ok) {
+    document.location.replace('/dashboard');
+  } else {
+    alert(response.statusText);
+  }
 };
 
 document.querySelector('.new-wine-form').addEventListener('submit', newWineHandler);
 
-  
-  
