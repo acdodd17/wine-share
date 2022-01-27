4812009a74973a7e5d610fc37b68dc11b9b0e047
Alvin Yearby <leroy.yearby@gmail.com>
2022-01-26 20:55:42 -0500

    added edit-post.js

--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -1,25 +1,39 @@
-// async function editFormHandler(event) {
-//     event.preventDefault();
+async function editFormHandler(event) {
+    event.preventDefault();
+
+    const wine_name = document.querySelector('input[name="wine-name"]').value;
+    const wine_type = document.querySelector('#wine-type option:checked').value;
+    const wine_vintage = document.querySelector('input[name="wine-vintage"]').value;
+    const wine_source = document.querySelector('input[name="wine-source"]').value;
+    const wine_notes = document.querySelector('input[name="wine-notes"]').value
   
-//     const title = document.querySelector('input[name="wine-notes"]').value.trim();
-//     const id = window.location.toString().split('/')[
-//       window.location.toString().split('/').length - 1
-//     ];
-//     const response = await fetch(`/api/posts/${id}`, {
-//       method: 'PUT',
-//       body: JSON.stringify({
-//         title
-//       }),
-//       headers: {
-//         'Content-Type': 'application/json'
-//       }
-//     });
+
   
-//     if (response.ok) {
-//       document.location.replace('/dashboard/');
-//     } else {
-//       alert(response.statusText);
-//     }
-// }
+    const id = window.location.toString().split('/')[
+      window.location.toString().split('/').length - 1
+    ];
+      
+      const response = await fetch(`/api/posts/${id}`, {
+        method: 'PUT',
+        body: JSON.stringify({
+          post_id: id,
+          wine_name,
+          wine_type,
+          wine_vintage,
+          wine_source,
+          wine_notes
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+      
+      if (response.ok) {
+        document.location.replace('/dashboard/');
+      } else {
+        alert(response.statusText);
+      }
   
-// document.querySelector('.edit-post-form').addEventListener('submit', editFormHandler);
+  }
+  
+  document.querySelector('.new-wine-form').addEventListener('submit', editFormHandler);
