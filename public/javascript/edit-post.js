acfd741afa923b8e7cfc479c47b1245996a5aaf9
Alvin Yearby <leroy.yearby@gmail.com>
2022-01-29 17:37:35 -0500

    edit notes & count works

--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -1,10 +1,6 @@
 async function editFormHandler(event) {
     event.preventDefault();
 
-    const wine_name = document.querySelector('input[name="wine-name"]').value;
-    const wine_type = document.querySelector('#wine-type option:checked').value;
-    const wine_vintage = document.querySelector('input[name="wine-vintage"]').value;
-    const wine_source = document.querySelector('input[name="wine-source"]').value;
     const wine_notes = document.querySelector('input[name="wine-notes"]').value;
     const wine_count = document.querySelector('input[name="wine-count"]').value;
 
@@ -17,10 +13,6 @@ async function editFormHandler(event) {
     const response = await fetch(`/api/posts/${id}`, {
         method: 'PUT',
         body: JSON.stringify({
-            wine_name,
-            wine_type,
-            wine_vintage,
-            wine_source,
             wine_notes, 
             wine_count
         }),
