c3afe82bb820e454779566a99d31d06570aefa01
Anna <acdodd17@gmail.com>
2022-01-30 16:24:17 -0500

    please work

--- public/javascript/wine-count.js
+++ public/javascript/wine-count.js
@@ -1,27 +1,27 @@
-async function upcountClickHandler(event) {
-    event.preventDefault();
+// async function upcountClickHandler(event) {
+//     event.preventDefault();
   
-    const id = window.location.toString().split('/')[
-      window.location.toString().split('/').length - 1
-    ];
-    const parsedId = parseInt(id);
-    const response = await fetch('/api/posts/quantity', {
-      method: 'PUT',
-      body: JSON.stringify({
-        post_id: parsedId,
-        count:2
-      }),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    });
+//     const id = window.location.toString().split('/')[
+//       window.location.toString().split('/').length - 1
+//     ];
+//     const parsedId = parseInt(id);
+//     const response = await fetch('/api/posts/quantity', {
+//       method: 'PUT',
+//       body: JSON.stringify({
+//         post_id: parsedId,
+//         count:2
+//       }),
+//       headers: {
+//         'Content-Type': 'application/json'
+//       }
+//     });
   
-    if (response.ok) {
-      //document.location.reload();
-      console.log(response)
-    } else {
-      alert(response.statusText);
-    }
-}
+//     if (response.ok) {
+//       //document.location.reload();
+//       console.log(response)
+//     } else {
+//       alert(response.statusText);
+//     }
+// }
   
-document.querySelector('.upcount-btn').addEventListener('click', upcountClickHandler);
+// document.querySelector('.upcount-btn').addEventListener('click', upcountClickHandler);
